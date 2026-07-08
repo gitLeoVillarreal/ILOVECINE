4d19e74604ed19f45426b559e1569a9175af8112
gitLeoVillarreal <lvillarrealfulgencio@gmail.com>
2026-07-08 06:10:58 +0000

    Functions that help the app

--- helpers.py
+++ helpers.py
@@ -1,35 +1,89 @@
 import os
-import requests
 import datetime
+import requests
+import sqlite3
 from dotenv import load_dotenv
+
 load_dotenv()
 api_key = os.getenv("TMDB_API_KEY")
 TMDB_BASE_URL = "https://api.themoviedb.org/3"
 
-def movieList():
-    currentDate = datetime.datetime.now()
+MAX_PAGE = 100
 
-    response = requests.get(
-        TMDB_BASE_URL + "/movie/changes",
+
+#MAKE A LIST OF ALL THE MOVIES TIL PAGE 100 APROX. 2000 MOVIES
+def MovieList():
+    movies = []
+    i = 1
+    today = datetime.date.today().isoformat()
+
+    while i <= MAX_PAGE:
+        response = requests.get(
+        TMDB_BASE_URL + f"/movie/popular?page={i}",
         params={"api_key": api_key}
+        )
+
+        response.raise_for_status()
+        
+        data = response.json()
+        movies.extend(data["results"])
+        
+
+        i += 1
+
+    movies = [
+        m for m in movies
+        if m.get("release_date") and m["release_date"] <= today
+    ]
+
+    sorted_movies = sorted(movies, key=lambda m: m["popularity"], reverse=True)
+    
+    return sorted_movies
+
+#SEARCH AND RETURN THE MOST VOTED POSTER OF A MOVIE BY MOVIE_ID
+
+def lookupPoster(poster):
+    if poster:
+        return f"https://image.tmdb.org/t/p/w500{poster}?"
+    else:
+        return "Not Found"
+
+#CREATES THE DATABASE FOR APP USAGE
+
+def MovieDataBase():
+    
+    conn = sqlite3.connect("database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("""
+    CREATE TABLE IF NOT EXISTS movies (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        movie_id INTEGER NOT NULL UNIQUE,
+        title TEXT NOT NULL,
+        poster_url TEXT NOT NULL,
+        genre_ids TEXT NOT NULL,
+        popularity NUMERIC NOT NULL,
+        rating NUMERIC NOT NULL,
+        release_date DATE NOT NULL
     )
-    data = response.json()
+""")
+    
+    moviesList = MovieList()
 
-    print(data["results"])
+    for m in moviesList:
 
-def lookup(name):
-    response = requests.get(
-        TMDB_BASE_URL + "/search/movie",
-        params={"api_key": api_key, "query": name}
-    )
-
-    data = response.json()
-    movies = data["results"]
-
-    movies_ordenados = sorted(movies, key=lambda m: m["popularity"], reverse=True)
-
-    return movies_ordenados
+        addMovie = (m["id"], m["title"], lookupPoster(m["poster_path"]), str(m["genre_ids"]), m["popularity"], m["vote_average"], m["release_date"])
+    
+        cursor.execute("INSERT OR IGNORE INTO movies (movie_id, title, poster_url, genre_ids, popularity, rating, release_date) VALUES (?,?,?,?,?,?,?)", addMovie)
+        
+    conn.commit()
+    conn.close()
+    print("Movies saved")
 
 
-#lookup("Inception")
-movieList()
+def main():
+    MovieDataBase()
+
+if __name__ == "__main__":
+    main()
+    
